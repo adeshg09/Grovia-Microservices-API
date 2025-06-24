2df3876418ffe45f3bc707f48ac973b90593b636
adesh09 <adeshgadage0908@gmail.com>
2025-06-25 02:18:13 +0530

    feat: Add logging for request query in getAllOutletDetails function

--- services/inventory-service/src/controllers/outlet.controller.ts
+++ services/inventory-service/src/controllers/outlet.controller.ts
@@ -86,6 +86,7 @@ export const getOutletDetailsByAdminId = async (
 
 export const getAllOutletDetails = async (req: Request, res: Response) => {
   try {
+    console.log("req.query", req.query);
     const token = req.headers.authorization?.split(" ")[1];
 
     if (!token) {
